81e8231c806a88e70b6ac108066f236098bc7386
Pierre Quebriac <pierre.quebriac@gmail.com>
2023-09-18 10:38:46 +0200

    lint and clean

--- js/libs/sound-functions.js
+++ js/libs/sound-functions.js
@@ -1,6 +1,6 @@
-const main = document.getElementsByTagName('main')[0];
+const main = document.getElementsByTagName("main")[0];
 
-// jouer un sample 
+// jouer un sample
 //playSample(fichier à lire, nombre de fois qu'il doit jouer en boucle [deault=1], interval in seconds, volume[default=1, de 0 à 1], pitch[default=1, de 0.3 à 3])
 function playSample(file, gain, pitch, loop, interval) {
   let URL = file;
@@ -9,22 +9,40 @@ function playSample(file, gain, pitch, loop, interval) {
   let playbackTime = interval !== undefined ? interval : 0;
   let audioGain = gain !== undefined ? gain : 1;
   let audioPitch = pitch !== undefined ? pitch : 1;
-  let id = slugify(file);
 
-  if(displayText){
-    let displaySample = document.createElement('p');
-    displaySample.classList.add("info");
-    displaySample.setAttribute('id', id);
-    let textToDisplay = 'function: playSample - file: ' + URL + ' - loop: '+ numLoops + ' - gain: '+ audioGain + '- pitch: ' + audioPitch + ' - interval: ' + playbackTime
-    displaySample.innerHTML = textToDisplay;
-    main.prepend(displaySample);
+  if (displayText) {
+    let id = slugify(file);
+    let textToDisplay =
+      "function: playSample - file: " +
+      URL +
+      " - loop: " +
+      numLoops +
+      " - gain: " +
+      audioGain +
+      "- pitch: " +
+      audioPitch +
+      " - interval: " +
+      playbackTime;
+
+    displayDebugText(textToDisplay, id);
   }
 
-  console.log('path:', URL, 'loop:', numLoops, 'gain:', audioGain, 'pitch:', audioPitch, 'time:', playbackTime);
+  console.log(
+    "path:",
+    URL,
+    "loop:",
+    numLoops,
+    "gain:",
+    audioGain,
+    "pitch:",
+    audioPitch,
+    "time:",
+    playbackTime
+  );
 
   function playLoopedAudio(audio, loopsLeft) {
     if (loopsLeft <= 0) {
-    	// document.getElementById(id).remove();
+      // document.getElementById(id).remove();
       return;
     }
 
@@ -43,10 +61,9 @@ function playSample(file, gain, pitch, loop, interval) {
   playLoopedAudio(audio, numLoops);
 }
 
-
 function playNote(wave, note, audioGain, duration, loop, tempo) {
   // Default values
-  let oscillatorType = wave !== undefined ? wave : 'sine';
+  let oscillatorType = wave !== undefined ? wave : "sine";
   let frequency = note !== undefined ? note : 440;
   let amplitude = audioGain !== undefined ? audioGain : 1;
   let audioDuration = duration !== undefined ? duration : 1;
@@ -54,142 +71,186 @@ function playNote(wave, note, audioGain, duration, loop, tempo) {
   let beatsPerMinute = tempo !== undefined ? tempo : 40;
   let secondsPerBeat = 60 / beatsPerMinute;
 
-  let id = wave + frequency + amplitude + audioDuration + numLoops + beatsPerMinute;
+  if (displayText) {
+    let textToDisplay =
+      "function: playNote - wave: " +
+      wave +
+      " - note: " +
+      frequency +
+      "- gain: " +
+      amplitude +
+      " - duration: " +
+      audioDuration +
+      " - loop: " +
+      numLoops +
+      " - tempo: " +
+      beatsPerMinute;
+    let id =
+      wave + frequency + amplitude + audioDuration + numLoops + beatsPerMinute;
 
-  if(displayText){
-    let displaySample = document.createElement('p');
-    displaySample.classList.add("info");
-    displaySample.setAttribute('id', id);
-    let textToDisplay = 'function: playNote - wave: ' + wave + ' - note: '+ frequency + '- gain: ' + amplitude + ' - duration: ' + audioDuration + ' - loop: '+ numLoops + " - tempo: " + beatsPerMinute; 
-    displaySample.innerHTML = textToDisplay;
-    main.prepend(displaySample);
+    displayDebugText(textToDisplay, id);
   }
 
   // Create web audio api context
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  let oscillator;
-  let gain;
-
-  function playOscillator(startTime) {
-    oscillator = audioCtx.createOscillator();
-    gain = audioCtx.createGain();
-
-    oscillator.type = oscillatorType;
-    oscillator.frequency.value = frequency;
-    gain.gain.value = amplitude;
-
-    oscillator.connect(gain);
-    gain.connect(audioCtx.destination);
-
-    oscillator.start(startTime);
-    oscillator.stop(startTime + audioDuration);
-    oscillator.onended = () => {
-    	// console.log('onended');
-      oscillator.disconnect();
-      gain.disconnect();      
-    };
-  }
 
   if (loop !== undefined && tempo !== undefined) {
+    console.log("here");
     for (let i = 0; i < numLoops; i++) {
-      const beatTime = audioCtx.currentTime + i * secondsPerBeat * audioDuration;
-      playOscillator(beatTime);
+      const beatTime =
+        audioCtx.currentTime + i * secondsPerBeat * audioDuration;
+      playOscillator(
+        audioCtx,
+        oscillatorType,
+        beatTime,
+        audioDuration,
+        amplitude,
+        frequency
+      );
     }
   } else {
-    playOscillator(audioCtx.currentTime); // Start the oscillator now
+    console.log("there");
+
+    playOscillator(
+      audioCtx,
+      oscillatorType,
+      audioCtx.currentTime,
+      audioDuration,
+      amplitude,
+      frequency
+    ); // Start the oscillator now
   }
 }
 
-
 function playMelody(wave, melody, audioGain, duration, loop, tempo) {
   // Default values
-  let oscillatorType = wave !== undefined ? wave : 'sine';
+  let oscillatorType = wave !== undefined ? wave : "sine";
   let amplitude = audioGain !== undefined ? audioGain : 1;
   let audioDuration = duration !== undefined ? duration : 1;
   let numLoops = loop !== undefined ? loop : 1;
   let beatsPerMinute = tempo !== undefined ? tempo : 40;
   let secondsPerBeat = 60 / beatsPerMinute;
 
-  let id = wave + melody[0] + amplitude + audioDuration + numLoops + beatsPerMinute;
+  if (displayText) {
+    let id =
+      wave + melody[0] + amplitude + audioDuration + numLoops + beatsPerMinute;
 
-  if(displayText){
-    let displaySample = document.createElement('p');
-    displaySample.classList.add("info");
-    displaySample.setAttribute('id', id);
-    let textToDisplay = 'function: playMelody - wave: ' + wave + ' - notes: '+ melody + '- gain: ' + amplitude + ' - duration: ' + audioDuration + ' - loop: '+ numLoops + " - tempo: " + tempo; 
-    displaySample.innerHTML = textToDisplay;
-    main.prepend(displaySample);
+    let textToDisplay =
+      "function: playMelody - wave: " +
+      wave +
+      " - notes: " +
+      melody +
+      "- gain: " +
+      amplitude +
+      " - duration: " +
+      audioDuration +
+      " - loop: " +
+      numLoops +
+      " - tempo: " +
+      tempo;
+
+    displayDebugText(textToDisplay, id);
   }
 
   // Create web audio api context
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-  function playOscillator(startTime, frequency) {
-    const oscillator = audioCtx.createOscillator();
-    const gain = audioCtx.createGain();
-
-    oscillator.type = oscillatorType;
-    oscillator.frequency.value = frequency;
-    gain.gain.value = amplitude;
-
-    oscillator.connect(gain);
-    gain.connect(audioCtx.destination);
-
-    oscillator.start(startTime);
-    oscillator.stop(startTime + audioDuration);
-    oscillator.onended = () => {
-      oscillator.disconnect();
-      gain.disconnect();
-    };
-  }
-
   let currentTime = audioCtx.currentTime;
 
   for (let i = 0; i < melody.length; i++) {
-    const frequency = melody[i];
+    const frequency = melody[i][0];
+    const duration = melody[i][1];
 
     if (loop !== undefined && tempo !== undefined) {
       for (let j = 0; j < numLoops; j++) {
         const beatTime = currentTime + j * secondsPerBeat * audioDuration;
-        playOscillator(beatTime, frequency);
+        playOscillator(
+          audioCtx,
+          oscillatorType,
+          beatTime,
+          audioDuration,
+          amplitude,
+          frequency
+        );
       }
     } else {
-      playOscillator(currentTime, frequency);
+      playOscillator(
+        audioCtx,
+        oscillatorType,
+        currentTime,
+        audioDuration,
+        amplitude,
+        frequency
+      );
     }
 
     currentTime += secondsPerBeat * audioDuration;
   }
 }
 
+// Play a predefine oscilator
+function playOscillator(
+  audioCtx,
+  oscillatorType,
+  startTime,
+  audioDuration,
+  amplitude,
+  frequency
+) {
+  const oscillator = audioCtx.createOscillator();
+  const gain = audioCtx.createGain();
+
+  oscillator.type = oscillatorType;
+  oscillator.frequency.value = frequency;
+  gain.gain.value = amplitude;
+
+  oscillator.connect(gain);
+  gain.connect(audioCtx.destination);
+
+  oscillator.start(startTime);
+  oscillator.stop(startTime + audioDuration);
+  oscillator.onended = () => {
+    oscillator.disconnect();
+    gain.disconnect();
+  };
+}
 
 // retourne un nombre aléatoire entre minimum et maximum
-function random(min, max){
-	min = Math.ceil(min);
+function random(min, max) {
+  min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 // retourne un nombre aléatoire entre plusieurs chiffres listés (merci RB)
-function shuffle(list){
-	if (!Array.isArray(list)) {    
+function shuffle(list) {
+  if (!Array.isArray(list)) {
     list = list.split(",");
   }
   let val = list[Math.floor(Math.random() * list.length)];
-  return val
+  return val;
+}
+
+function displayDebugText(textToDisplay, id) {
+  let displaySample = document.createElement("p");
+  displaySample.classList.add("info");
+  displaySample.setAttribute("id", id);
+  displaySample.innerHTML = textToDisplay;
+  main.prepend(displaySample);
 }
 
 // function shuffle(list, chances) {
-//   if (!Array.isArray(list)) {    
+//   if (!Array.isArray(list)) {
 //     list = list.split(",");
 //   }
-  
+
 //   if(chances){
 //     // Calculate the total chance (sum of all chances)
 //     const totalChance = Object.values(chances).reduce((sum, chance) => sum + chance, 0);
-    
+
 //     // Generate a random number between 0 and the totalChance
 //     const randomValue = Math.random() * totalChance;
-    
+
 //     let cumulativeChance = 0;
 //     for(let i=0; i<list.length; i++){
 //       const chance = chances[list[i]];
@@ -209,7 +270,7 @@ function shuffle(list){
 //     //     return value;
 //     //   }
 //     // }
-    
+
 //     // // Return null if no value is selected (shouldn't normally happen unless chances don't add up to 1)
 //     // return null;
 //   }
@@ -219,17 +280,12 @@ function shuffle(list){
 //   }
 // }
 
-
 // Slugify a string
 function slugify(str) {
   return str
-    .toLowerCase()                 // Convert to lowercase
-    .replace(/[^\w\s-]/g, '')      // Remove non-word characters
-    .replace(/\s+/g, '-')          // Replace spaces with hyphens
-    .replace(/--+/g, '-')          // Replace multiple hyphens with a single hyphen
-    .trim();                       // Trim any leading/trailing whitespace
+    .toLowerCase() // Convert to lowercase
+    .replace(/[^\w\s-]/g, "") // Remove non-word characters
+    .replace(/\s+/g, "-") // Replace spaces with hyphens
+    .replace(/--+/g, "-") // Replace multiple hyphens with a single hyphen
+    .trim(); // Trim any leading/trailing whitespace
 }
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -3,103 +3,140 @@ let displayText = true;
 // launch init fuction
 init();
 
-async function init(){
+async function init() {
   return new Promise((resolve) => {
-    document.body.addEventListener('keydown', handleKeyDown);
-    console.log('Welcome to funcsounds');
-    resolve('init-done');
-  })
+    document.body.addEventListener("keydown", handleKeyDown);
+    console.log("Welcome to funcsounds");
+    resolve("init-done");
+  });
 }
 
-function handleKeyDown(e){
-
-  if(e.key == 'n'){
-    playNote("sine", 200, 0.1, 0.2, 10, 30);
+function handleKeyDown(e) {
+  if (e.key == "n") {
+    playNote("sine", 200, 0.81, 0.2, 10, 30);
     changeBackground(shuffle("aquamarine, pink, yellow, brown, chartreuse"));
-    addVideo("images/station.mp4", random(100, 500), random(100, 500), 200, true);
+    addVideo(
+      "images/station.mp4",
+      random(100, 500),
+      random(100, 500),
+      200,
+      true
+    );
   }
 
-  if(e.key == 'b'){
+  if (e.key == "b") {
     playSample("samples/over_3.mp3", 1, 1);
     addImage("images/chamber-of-ecstasy.png", 0, 0, 1280);
   }
 
-  if(e.key == 'v'){
+  if (e.key == "v") {
     playMelody("sawtooth", [100, 200, 100, 200, 150, 200], 0.1, 0.1);
     addText("YOUPI", 100, random(0, 800), random(0, 1280), "black");
-    messyEffect(['p', 'figure'], -5, 5);
+    messyEffect(["p", "figure"], -5, 5);
   }
 
-
-
-  if(e.key == 'a'){
+  if (e.key == "a") {
     playSample("samples/over_3.mp3", 1, 1);
-    addText("TUUUUUUT", 40, random(0, 800), random(0, 1280), shuffle('aquamarine, lightblue, yellow, blue, pink, magenta'));
+    addText(
+      "TUUUUUUT",
+      40,
+      random(0, 800),
+      random(0, 1280),
+      shuffle("aquamarine, lightblue, yellow, blue, pink, magenta")
+    );
   }
 
-  if(e.key == 'z'){
+  if (e.key == "z") {
     playSample("samples/over_4.mp3", 1, 1);
-    addText(shuffle("il, revient, olala"), 80, random(0, 800), random(0, 1280), "black");
+    addText(
+      shuffle("il, revient, olala"),
+      80,
+      random(0, 800),
+      random(0, 1280),
+      "black"
+    );
   }
 
-  if(e.key == 'e'){
+  if (e.key == "e") {
     playNote("triangle", 100, 0.5, 1);
   }
 
-  if(e.key == 't'){
+  if (e.key == "t") {
     playNote("sine", 120, 0.5, 1, 3, 50);
   }
 
-  if(e.key == 'y'){
+  if (e.key == "y") {
     playNote("sine", 50, 1, 3);
-    messyEffect(['p', 'figure'], -15, 15);
+    messyEffect(["p", "figure"], -15, 15);
   }
 
-  if(e.key == 'u'){
-    playMelody('sine', [440, 523.25, 659.26], 0.5, 0.6);
+  if (e.key == "u") {
+    playMelody("sine", [440, 523.25, 659.26], 0.5, 0.6);
   }
 
-  if(e.key == 'i'){
-    playMelody('triangle', [100, 150, 100, 150, 120, 150], 0.4, 0.2);
+  if (e.key == "i") {
+    playMelody("triangle", [100, 150, 100, 150, 120, 150], 0.4, 0.2);
   }
 
-  if(e.key == 'o'){
+  if (e.key == "o") {
     playNote("triangle", 400, 0.1, 0.2, 60, 10);
   }
 
-  if(e.key == 'p'){
+  if (e.key == "p") {
     playNote("sine", random(50, 120), 1, 0.5);
   }
 
-  if(e.key == 'q'){
-    playMelody('triangle', [shuffle("100,150,120"), shuffle("100,150,120"), shuffle("100,150,120"), shuffle("100,150,120"), shuffle("100,150,120"), shuffle("100,150,120")], 0.4, 0.2);
+  if (e.key == "q") {
+    playMelody(
+      "triangle",
+      [
+        shuffle("100,150,120"),
+        shuffle("100,150,120"),
+        shuffle("100,150,120"),
+        shuffle("100,150,120"),
+        shuffle("100,150,120"),
+        shuffle("100,150,120"),
+      ],
+      0.4,
+      0.2
+    );
   }
 
-  if(e.key == 'l'){
-    playNote(shuffle('triangle, sine, sawtooth, square'), 172, 0.9, 0.4);
+  if (e.key == "l") {
+    playNote(shuffle("triangle, sine, sawtooth, square"), 172, 0.9, 0.4);
   }
 
-  if(e.key == 'm'){
-    playNote(shuffle('triangle, sine, sawtooth, square'), 172, 0.6, 0.4);
+  if (e.key == "m") {
+    playNote(shuffle("triangle, sine, sawtooth, square"), 172, 0.6, 0.4);
   }
-
 
   // functions visuelles
 
-  if(e.key == 'w'){
-    addImage(shuffle('images/bush_02.png, images/flower_01.png, images/flower_02.png, images/grasses_01.png, images/plant_01.png, images/plant_02.png'), random(0, 800), random(0, 1280), random(100, 400));
+  if (e.key == "w") {
+    addImage(
+      shuffle(
+        "images/bush_02.png, images/flower_01.png, images/flower_02.png, images/grasses_01.png, images/plant_01.png, images/plant_02.png"
+      ),
+      random(0, 800),
+      random(0, 1280),
+      random(100, 400)
+    );
   }
 
-  if(e.key == 'x'){
-    changeBackground(shuffle('aquamarine, lightblue, yellow, blue, pink, magenta'));
+  if (e.key == "x") {
+    changeBackground(
+      shuffle("aquamarine, lightblue, yellow, blue, pink, magenta")
+    );
   }
 
-  if(e.key == 'c'){
-    addImage(shuffle('images/birds.png, images/chamber-of-ecstasy.png, images/clouds02.png, images/ring.png'), random(0, 800), random(0, 1280), random(100, 400));
+  if (e.key == "c") {
+    addImage(
+      shuffle(
+        "images/birds.png, images/chamber-of-ecstasy.png, images/clouds02.png, images/ring.png"
+      ),
+      random(0, 800),
+      random(0, 1280),
+      random(100, 400)
+    );
   }
-
 }
-
-
-
-
